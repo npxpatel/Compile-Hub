70391f397f6e4049ee90d1c7a80243b6670eee77
btrezzy <128296263+btrezzy@users.noreply.github.com>
2024-07-07 01:07:07 +0530

    Fix missing type declarations and updates

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="w-full h-[calc(h-screen-60px)] flex flex-col gap-4 justify-center items-center ">
      
-      <h1 className="text-6xl text-center font-bold pt-40 ">WD Compiler</h1>
-      <p className="text-center text-gray-500"> Compile HTML, CSS, Javascript code on the flow and save it on for later! </p>
+      <h1 className="text-4xl md:text-6xl text-center font-bold pt-40 ">WD Compiler</h1>
+      <p className=" text-sm md:text-lg text-center text-gray-500"> Compile HTML, CSS, Javascript code on the flow and save it on for later! </p>
 
 
 
