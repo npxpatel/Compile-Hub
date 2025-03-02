2446ff10f08a3bfbbfdf332dc265e76153630cbf
npxpatel <nirajkumarpatel21@gmail.com>
2025-03-02 16:33:27 +0530

    vercel configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,7 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,4 +9,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    host: true,
+    port: 5173,
+  },
+  preview: {
+    port: 4173,
+  }
+});
